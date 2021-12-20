60bb2278aee2b97e160e9b63b66fdc422ca89a7a
ANGELA ANN JAISON <74919017+angelaannjaison@users.noreply.github.com>
2021-12-20 14:36:26 +0530

    Ex_5b

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button clickButton = findViewById(R.id.button);
+        final TextView myText = findViewById(R.id.textView);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myText.setText("Hello all...");
+            }
+        });
     }
 }
