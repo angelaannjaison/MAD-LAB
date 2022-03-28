d4fd7c2754157de3a22180a12ea456951dc425a3
ANGELA ANN JAISON <74919017+angelaannjaison@users.noreply.github.com>
2022-03-28 20:58:06 +0530

    Update MainActivity.java

--- take input of 2 num from pg 1 and display res on next pg/MainActivity.java
+++ take input of 2 num from pg 1 and display res on next pg/MainActivity.java
@@ -1,36 +1,36 @@
-package com.example.testing;
+package com.example.appapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
-    Button b;
-
+    Button btn;
+    EditText ed1;
+    EditText ed2;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b=(Button) findViewById(R.id.btn);
 
-        b.setOnClickListener(new View.OnClickListener()
+        btn=(Button) findViewById(R.id.button3);
+        ed1=(EditText) findViewById(R.id.ed1);
+        ed2=(EditText) findViewById(R.id.ed2);
 
-        {
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent i=new Intent(MainActivity.this,MainActivity2.class);
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                i.putExtra("val1",ed1.getText().toString());
+                i.putExtra("val2",ed2.getText().toString());
                 startActivity(i);
-
             }
         });
     }
