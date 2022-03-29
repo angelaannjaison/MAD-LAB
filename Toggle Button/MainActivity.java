61cbc4b3d7ed09bb7def24073641d068bc9054bf
ANGELA ANN JAISON <74919017+angelaannjaison@users.noreply.github.com>
2022-03-29 17:56:36 +0530

    Update MainActivity.java

--- Toggle Button/MainActivity.java
+++ Toggle Button/MainActivity.java
@@ -1,33 +1,26 @@
-package com.example.login;
+package com.example.toggle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity {
 
-    ToggleButton tb;
+    ToggleButton toggleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tb=(ToggleButton) findViewById(R.id.toggleButton);
-        tb.setOnCheckedChangeListener(this);
+        toggleButton=(ToggleButton) findViewById(R.id.toggleButton);
     }
 
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
-    if(compoundButton.getId()==R.id.toggleButton) {
-        if(b) Toast.makeText(this, "Toggle Button :Switched on", Toast.LENGTH_SHORT).show();
+    public void toggle(View view) {
+        if(toggleButton.isChecked())Toast.makeText(this, "Toggle Button :Switched on", Toast.LENGTH_SHORT).show();
         else Toast.makeText(this, "Toggle Button :Switched off", Toast.LENGTH_SHORT).show();
-    }
+
     }
 }
